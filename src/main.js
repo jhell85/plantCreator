da95c83c19b4ec5ed293645fb94016e65f3a7dcb
jhell85 <jhell85@gmail.com>
2020-06-23 15:10:58 -0700

    add state for plants and seperated logic from back and front end

--- src/main.js
+++ src/main.js
@@ -2,3 +2,58 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { storeState, changeState } from './plantFactory.js';
+
+
+// // This function stores our state.
+
+// const storeState = () => {
+//   let currentState = {};
+//   return (stateChangeFunction) => {
+//     const newState = stateChangeFunction(currentState);
+//     currentState = {...newState};
+//     return newState;
+//   }
+// }
+
+
+
+// // This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
+// const changeState = (prop) => {
+//   return (value) => {
+//     return (state) => ({
+//       ...state,
+//       [prop] : (state[prop] || 0) + value
+//     })
+//   }
+// }
+
+// We create two functions using our function factory. We could easily create many more.
+
+const redFood = changeState("soil")(-2);
+const blueFood = changeState("soil")(5);
+const greenFood = changeState("soil")(7);
+const yellowFood = changeState("soil")(-1);
+const stateChanger = storeState();
+
+$(document).ready(function() {
+
+  // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect.
+  $('#feedBlue').click(function() {
+    const myFedPlant = stateChanger(blueFood);
+    $('#soil-value').text(myFedPlant.soil);
+  });
+  $('#feedGreen').click(function() {
+    const myFedPlant = stateChanger(greenFood);
+    $('#soil-value').text(myFedPlant.soil);
+  });
+  $('#feedYellow').click(function() {
+    const myFedPlant = stateChanger(yellowFood);
+    $('#soil-value').text(myFedPlant.soil);
+  });
+  $('#feedRed').click(function() {
+    const myFedPlant = stateChanger(redFood);
+    $('#soil-value').text(myFedPlant.soil);
+  });
+});
--- src/plantFactory.js
+++ src/plantFactory.js
@@ -1,31 +1,52 @@
+export { storeState };
+export { changeState };
+
+//how to store specific changes
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
 //state modifier function
 const changeState = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
       [prop]: (state[prop] || 0) + value
-    })
-  }
+    });
+  };
 }
-//Function Factory
-const feed = changeState("fertilizer");
-//feed(3)(plant);
-//feed(5)(tree);
-const addWater = changeState("water");
-//addWater(2)(plant);
-//addWater(200)(pool);
-const foliate = changeState("leaves");
-const growFruit = changeState("berries");
-const branchOut = changeState("branches")(1);
-//branchOut(plant);
-//branchOut(tree);
-const prune = changeState("branches")(-1);
-//prune(tree);
-const compost = changeState("fertilizer")(2);
-//compost(plant);
-const miracleGro = changeState("fertilizer")(5);
+//Functions in Function Factory
+// const feed = changeState("soil");
+// const blueFood = changeState("soil")(5);
+
+
+// //Julia's elaborate Function Factory
+// const feed = changeState("fertilizer");
+// //feed(3)(plant);
+// //feed(5)(tree);
+// const addWater = changeState("water");
+// //addWater(2)(plant);
+// //addWater(200)(pool);
+// const foliate = changeState("leaves");
+// const growFruit = changeState("berries");
+// const branchOut = changeState("branches")(1);
+// //branchOut(plant);
+// //branchOut(tree);
+// const prune = changeState("branches")(-1);
+// //prune(tree);
+// const compost = changeState("fertilizer")(2);
+// //compost(plant);
+// const miracleGro = changeState("fertilizer")(5);
 
 //storing state
+
+// let plant = { branches: 1, berries: 0, leaves: 1, flowers: 0}
+
 
 
 
